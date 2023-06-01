285218d5fd600dc3eaadd4d03820782a08fa4b17
Koifman <primeless42@gmail.com>
2023-06-01 10:51:17 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,59 +2,63 @@ import openai
 import requests
 import yaml
 import json
+import subprocess
+import time
 
-openai.api_key = "ENTER YOUR KEY HERE"
 
-# Function to retrieve SIGMA rule from a YAML file
+openai.api_key = "ENTER_KEY_HERE"
+
+
 def get_sigma_rule_from_yaml(yaml_url):
-    # Send HTTP GET request to retrieve the YAML file
     response = requests.get(yaml_url)
-    
-    # Parse the YAML content and extract the SIGMA rule
     sigma_rule = yaml.safe_load(response.text)
-    
     return sigma_rule
 
-# Function to generate CMD command from a SIGMA rule
+
 def generate_cmd_command(sigma_rule):
-    # Convert the SIGMA rule to a string
     sigma_rule_str = yaml.dump(sigma_rule)
-    
-    # Send the SIGMA rule as a prompt to the API
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
-            {"role":"system","content":"provide CMD commands that demonstrate the behavior described in the following sigma rule"+sigma_rule_str}
+            {"role":"system","content":"provide CMD commands that demonstrate the behavior described in the following sigma rule"+sigma_rule_str + ". Please make sure to give me a list of ONLY the commands, without ANY other text. Also, please include commands to REVERT any creations or modifications from the previous commands. Again, list ONLY commands, without any other explanation text whatsoever. Also, please include a number prefix for each command you include, for example: 1. <Command>"}
         ],
         max_tokens=1000,
         n=1,
         stop=None,
-        temperature=0.7
+        temperature=0.3
     )
     
-    # Extract the generated CMD command from the API response
+
     cmd_command = response.choices[0].message["content"]
-    #formatted = json.dumps(cmd_command, indent=4)
-    
     return cmd_command
 
-# Main interaction loop
+def run_commands(output):
+    output = output.split("\n")
+    commands_to_run = [line.split('. ', 1)[1] for line in output if '. ' in line]
+    print("Running the following commands:\n")
+    for command in commands_to_run:
+        print(command + "\n")
+    print("\n\n") 
+    for command in commands_to_run:
+        print("[*] Running: " + command + "\n")
+        completed_process = subprocess.run(command, shell=True, capture_output=True, text=True)
+        print(completed_process.stdout)
+        time.sleep(2)
+
+
 while True:
-    # Get YAML URL input from the user
     yaml_url = input("Enter the URL of a SIGMA rule in YAML format (or 'quit' to exit): ")
-    
-    # Check if user wants to exit
     if yaml_url.lower() == 'quit':
         break
     
-    # Retrieve the SIGMA rule from the YAML file
+
     sigma_rule = get_sigma_rule_from_yaml(yaml_url)
-    
-    
-    # Generate CMD command from the SIGMA rule
     cmd_command = generate_cmd_command(sigma_rule)
     
-    # Display the generated CMD command
     print("Generated CMD command:")
     print(cmd_command)
-    print()
+    ifrun = input("Would you like to execute the commands? Y/N\n")
+    if ifrun == "Y" or ifrun == "y":
+        commands_list = run_commands(cmd_command)
+    else:
+        break
